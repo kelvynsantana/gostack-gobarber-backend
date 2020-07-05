e34c766efef38ba8ea851f1a0cf197467148307b
kelvynsantana <santanakelvyn@gmail.com>
2020-07-05 18:43:44 -0300

    Add Authentication

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '4ba95760612a3197878a9c8079086d23',
+    expiresIn: '1d',
+  },
+};
--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -5,7 +5,10 @@ import { getCustomRepository } from 'typeorm';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 import CreateAppointmentService from '../services/CreateAppointmentService';
 
+import ensureAuthenticated from '../middlewares/ensureauthenticated';
+
 const appointmentsRouter = Router();
+appointmentsRouter.use(ensureAuthenticated);
 
 appointmentsRouter.get('/', async (request, response) => {
   const appointmentsRepository = getCustomRepository(AppointmentsRepository);
